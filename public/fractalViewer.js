857c0283783db9484f775958a463712b0231f8c1
User <user@example.com>
2025-04-15 14:00:09 +0200

    Optimize 2D fractal rendering: use color LUT and Uint32Array buffer for fast per-pixel writes

--- public/fractalViewer.js
+++ public/fractalViewer.js
@@ -53,6 +53,10 @@ FractalViewer.prototype.setColorOffset = function(offset) {
   this.render();
 };
 
+FractalViewer.prototype.setOnViewChange = function(cb) {
+  this.onViewChange = cb;
+};
+
 FractalViewer.prototype.setupEvents = function() {
   this.canvas.addEventListener('mousedown', e => {
     this.dragging = true;
@@ -71,6 +75,7 @@ FractalViewer.prototype.setupEvents = function() {
     this.view.centerY -= dy * scale / this.height;
     this.render();
     if (this.infoCallback) this.infoCallback(this.view);
+    if (this.onViewChange) this.onViewChange(this.view);
   });
   window.addEventListener('mouseup', () => {
     if (this.dragging) {
@@ -85,6 +90,7 @@ FractalViewer.prototype.setupEvents = function() {
     this.view.scale *= zoom;
     this.render();
     if (this.infoCallback) this.infoCallback(this.view);
+    if (this.onViewChange) this.onViewChange(this.view);
   });
   this.canvas.style.cursor = 'grab';
 };
@@ -95,24 +101,29 @@ FractalViewer.prototype.render = function() {
     this.ctx.fillRect(0, 0, this.width, this.height);
     return;
   }
-  // Render with magnification for missing data
+  // --- FAST COLOR LOOKUP TABLE OPTIMIZATION ---
+  // Precompute color LUT for all possible iteration values
+  const lut = new Uint32Array(this.maxIter + 2); // +1 for maxIter (black), +1 for -1 (uncalculated)
+  const toRGBA = (r, g, b, a=255) => (a << 24) | (b << 16) | (g << 8) | r;
+  for (let i = 0; i <= this.maxIter; ++i) {
+    if (i === this.maxIter) {
+      lut[i] = toRGBA(0, 0, 0, 255); // Inside set = black
+    } else {
+      const color = this.iterToColor(i, this.maxIter);
+      lut[i] = toRGBA(color[0], color[1], color[2], 255);
+    }
+  }
+  lut[this.maxIter + 1] = toRGBA(40, 40, 40, 255); // -1 (uncalculated)
+
+  // Use Uint32Array view for fast pixel writes
   const img = this.ctx.createImageData(this.width, this.height);
-  for (let y = 0; y < this.height; ++y) {
-    for (let x = 0; x < this.width; ++x) {
-      const idx = y * this.width + x;
-      const iter = this.imageData[idx];
-      let color;
-      if (iter === -1) {
-        // Uncalculated: show as dark gray
-        color = [40, 40, 40];
-      } else {
-        color = this.iterToColor(iter, this.maxIter);
-      }
-      const p = idx * 4;
-      img.data[p] = color[0];
-      img.data[p+1] = color[1];
-      img.data[p+2] = color[2];
-      img.data[p+3] = 255;
+  const buf32 = new Uint32Array(img.data.buffer);
+  for (let i = 0; i < this.width * this.height; ++i) {
+    const iter = this.imageData[i];
+    if (iter === -1) {
+      buf32[i] = lut[this.maxIter + 1];
+    } else {
+      buf32[i] = lut[iter];
     }
   }
   this.ctx.putImageData(img, 0, 0);
